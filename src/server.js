9f0b04ec9056bad2db7ed326f8a3ea8061fb4465
Albert_Leipzig <albertclemvill@gmail.com>
2024-02-06 22:35:13 +0100

    Albert : called routers in server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { faker } from '@faker-js/faker';
-import mongoose from 'mongoose';
 import { connectToDb } from './utils/connectToDb.js';
+
+import { busRouter } from './routers/busRouter.js';
 import { customerRouter } from './routers/customerRouter.js';
+import { employeeRouter } from './routers/employeeRouter.js';
+import { routeRouter } from './routers/routeRouter.js';
+import { stopRouter } from './routers/stopRouter.js';
+import { travelCustomerRouter } from './routers/travelCustomerRouter.js';
+import { travelRouter } from './routers/travelRouter.js';
+
 import { addFakeData, deleteAllFakeData } from './utils/addFakeData.js';
 dotenv.config();
 
@@ -14,19 +20,13 @@ const PORT = process.env.PORT || 3030;
 
 app.use(express.json());
 
-const PersonSchema = new mongoose.Schema(
-  {
-    firstName: { type: String },
-    lastName: { type: String },
-    email: { type: String },
-  },
-  {
-    collection: 'persons',
-    versionKey: false,
-  }
-);
-
+app.use('/bus', busRouter);
 app.use('/customer', customerRouter);
+app.use('/employee', employeeRouter);
+app.use('/route', routeRouter);
+app.use('/stop', stopRouter);
+app.use('/travelCustomer', travelCustomerRouter);
+app.use('/travel', travelRouter);
 app.route('/fake').post(addFakeData).delete(deleteAllFakeData);
 
 const startServer = async () => {
