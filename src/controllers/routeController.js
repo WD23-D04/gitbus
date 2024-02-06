643e86f5f4e527fe951253e4e037ae1946ee7efd
Albert_Leipzig <albertclemvill@gmail.com>
2024-02-06 22:03:27 +0100

    Albert : added routeController

---
+++ src/controllers/routeController.js
@@ -0,0 +1,60 @@
+import { Route } from '../schemas/RouteSchema.js';
+
+export const addSingleRoute = async (req, res) => {
+  try {
+    const rtoute = await Route.create(req.body);
+    res.status(201).json(route);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
+
+export const getSingleRoute = async (req, res) => {
+  try {
+    const route = await Route.findById(req.params.id);
+    route
+      ? res.status(200).json(route)
+      : res
+          .status(404)
+          .json({ mgs: `Route with id ${req.params.id} not found` });
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
+
+export const getAllRoutes = async (req, res) => {
+  try {
+    const routes = await Route.find();
+    res.status(200).json(routes);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
+
+export const updateSingleRoute = async (req, res) => {
+  try {
+    const route = await Route.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    route
+      ? res.status(200).json(route)
+      : res
+          .status(404)
+          .json({ mgs: `Route with id ${req.params.id} not found` });
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
+
+export const deleteSingleRoute = async (req, res) => {
+  try {
+    const route = await Route.findByIdAndDelete(req.params.id);
+    route
+      ? res.status(200).json(route)
+      : res
+          .status(404)
+          .json({ mgs: `Route with id ${req.params.id} not found` });
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
