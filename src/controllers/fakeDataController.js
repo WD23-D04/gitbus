06852a4dc09eae9570e32bd32df3a621543c6597
AlbertLeipzig <albertclemvill@gmail.com>
2024-02-06 19:02:04 +0100

    finished

--- src/controllers/fakeDataController.js
+++ src/controllers/fakeDataController.js
@@ -2,30 +2,14 @@ import { faker } from '@faker-js/faker';
 import { Customer } from '../schemas/customerSchema.js';
 export const fakeDataController = async (req, res) => {
   try {
-    let customersData = [];
-
-    for (let i = 0; i < 10; i++) {
-      const customer = {
+    const customers = await Customer.insertMany(
+      Array.from({ length: 100 }, () => ({
         firstName: faker.person.firstName(),
         lastName: faker.person.lastName(),
         email: faker.internet.email(),
         personalId: faker.string.uuid(),
         baggage: faker.helpers.rangeToNumber({ min: 0 }, { max: 20 }),
-      };
-      customersData.push(customer);
-    }
-
-    const customers = await Customer.insertMany(
-      Array.from(
-        { length: 100 },
-        {
-          firstName: faker.person.firstName(),
-          lastName: faker.person.lastName(),
-          email: faker.internet.email(),
-          personalId: faker.string.uuid(),
-          baggage: faker.helpers.rangeToNumber({ min: 0 }, { max: 20 }),
-        }
-      )
+      }))
     );
 
     res.status(201).json(customers);
